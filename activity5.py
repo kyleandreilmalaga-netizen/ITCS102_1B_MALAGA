38eb03da992c48be445ba3d493b6e9e8747be97a
kyleandreilmalaga-netizen <kyleandreilmalaga@gmail.com>
2026-08-26 11:07:06 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,11 @@
+#operators activity
+
+a = 13
+b = 9
+sum = a + b
+diff = a - b
+product = a*b
+
+print("The sum of", a, "and", b, "is", sum)
+print("The different of", a, "and", b, "is", diff)
+print("The product of ", a, "and", b, "is", product)
