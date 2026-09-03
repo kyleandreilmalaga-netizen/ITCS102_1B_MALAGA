3d6c0ef14c8d3f10a6372876f4a8f5138c37e796
kyleandreilmalaga-netizen <kyleandreilmalaga@gmail.com>
2026-09-03 09:43:18 +0800

    Add files via upload

---
+++ activity6.py
@@ -0,0 +1,9 @@
+#activity 6 
+#dynamic, static
+#dynamic - ever changing 
+#static - fixed
+
+name = input("What is your name? -->")
+print("Hello", name)
+age = input("How old are you?-->")
+print("Owh that's great!")
