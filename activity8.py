70a8a709899b0ec1b719f2561db562fb6698a88c
kyleandreilmalaga-netizen <kyleandreilmalaga@gmail.com>
2026-09-11 10:31:10 +0800

    Add files via upload

---
+++ activity8.py
@@ -0,0 +1,40 @@
+#activity 8 
+x = ""
+
+food = input("What is your favorite foods -->>")
+x += food + ","
+
+food = input("What else? -->>")
+x += food + ","
+
+food = input("What else? -->>")
+x += food + ","
+
+food = input("What else? -->>")
+x += food + ","
+
+food = input("What else? -->>")
+x += food + ","
+
+food = input("What else? -->>")
+x += food + ","
+
+food = input("What else? -->>")
+x += food + ","
+
+food = input("What else? -->>")
+x += food + ","
+
+food = input("What else? -->>")
+x += food + ","
+
+food = input("What else? -->>")
+x += food + ","
+
+food = input("What else? -->>")
+x += food + "and"
+
+food = input("What else? -->>")
+x += food 
+
+print("My favorite foods are", x)
